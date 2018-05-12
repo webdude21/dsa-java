cc1d1aa81fda11a3d18200fe3ed15bdb4570c66b
webdude <dimo.petrof@gmail.com>
2018-05-12 12:42:25 +0300

    clean up atm tests

--- src/test/java/eu/webdude/dsa/algo/AtmMachineTest.java
+++ src/test/java/eu/webdude/dsa/algo/AtmMachineTest.java
@@ -3,6 +3,7 @@ package eu.webdude.dsa.algo;
 import eu.webdude.dsa.algo.atm.AtmMachine;
 import eu.webdude.dsa.algo.atm.Bill;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -29,32 +30,34 @@ public class AtmMachineTest implements ArgumentsProvider {
   public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
     return Stream.of(
       Arguments.of(bills, 8, Collections.singletonList(Bill.of(2, 4))),
-      Arguments.of(bills, 7, Arrays.asList(Bill.of(2, 1), Bill.of(5, 1))),
+      Arguments.of(bills, 7, Arrays.asList(Bill.of(5, 1), Bill.of(2, 1))),
       Arguments.of(bills, 17, Arrays.asList(Bill.of(10, 1), Bill.of(5, 1), Bill.of(2, 1))),
-      Arguments.of(bills, 18, Arrays.asList(Bill.of(10, 1), Bill.of(2, 4)))
+      Arguments.of(bills, 18, Arrays.asList(Bill.of(10, 1), Bill.of(2, 4))),
+      Arguments.of(bills, 118, Arrays.asList(Bill.of(100, 1), Bill.of(10, 1), Bill.of(2, 4)))
     );
+  }
+
+  @BeforeEach
+  void setUp() {
+
   }
 
   @ParameterizedTest
   @ArgumentsSource(AtmMachineTest.class)
   void testForCertainBillSizes(List<Bill> bills, int takeOutAmount, List<Bill> expected) {
-    AtmMachine atmMachine = new AtmMachine(bills);
+    var atmMachine = new AtmMachine(bills);
 
-    Collections.sort(expected);
-
-    List<Bill> actualBills = atmMachine.takeOut(takeOutAmount);
+    var actualBills = atmMachine.takeOut(takeOutAmount);
 
     Assertions.assertArrayEquals(actualBills.toArray(), expected.toArray());
   }
 
   @ParameterizedTest
   @ArgumentsSource(AtmMachineTest.class)
-  void testIfAmountIsOk(List<Bill> bills, int takeOutAmount, List<Bill> expected) {
-    AtmMachine atmMachine = new AtmMachine(bills);
+  void testIfAmountIsOk(List<Bill> bills, int takeOutAmount) {
+    var atmMachine = new AtmMachine(bills);
 
-    Collections.sort(expected);
-
-    int actual = atmMachine.takeOut(takeOutAmount).stream().mapToInt(Bill::getTotal).sum();
+    var actual = atmMachine.takeOut(takeOutAmount).stream().mapToInt(Bill::getTotal).sum();
 
     Assertions.assertEquals(takeOutAmount, actual);
   }
