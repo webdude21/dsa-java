e27c80e4de5a69cbabf4777f8b3055511f5cdb07
webdude <dimo.petrof@gmail.com>
2018-05-30 20:03:43 +0300

    convert to use implicit types

--- src/main/java/eu/webdude/dsa/projecteuler/LargestPalindromeProduct.java
+++ src/main/java/eu/webdude/dsa/projecteuler/LargestPalindromeProduct.java
@@ -3,13 +3,13 @@ package eu.webdude.dsa.projecteuler;
 class LargestPalindromeProduct {
 
   static long solve(int digitsInProduct) {
-    long upperBound = (long) Math.pow(10, digitsInProduct) - 1;
-    long lowerBound = (long) (upperBound - Math.pow(10, digitsInProduct - 1)) - 1;
+    var upperBound = (long) Math.pow(10, digitsInProduct) - 1;
+    var lowerBound = (long) (upperBound - Math.pow(10, digitsInProduct - 1)) - 1;
     long maxPalindrome = -1;
 
-    for (long a = upperBound; a >= lowerBound; a--) {
-      for (long b = upperBound; b >= lowerBound; b--) {
-        long product = a * b;
+    for (var a = upperBound; a >= lowerBound; a--) {
+      for (var b = upperBound; b >= lowerBound; b--) {
+        var product = a * b;
         if (isPalindrome(String.valueOf(product))) {
           maxPalindrome = Math.max(maxPalindrome, product);
         }
