728b0d39f58eb720e302fc9dab7075c4398b620f
webdude <dimo.petrof@gmail.com>
2018-06-15 18:47:55 +0300

    solve largest product in a grid

---
+++ src/main/java/eu/webdude/dsa/projecteuler/LargestProductInaGrid.java
@@ -0,0 +1,58 @@
+package eu.webdude.dsa.projecteuler;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+class LargestProductInaGrid {
+
+  static int largestProduct(int[][] testData) {
+    return genericSolve(testData, 4, (a, b) -> a * b, Math::max, Integer.MIN_VALUE);
+  }
+
+  private static int genericSolve(int[][] input, int lineSize,
+                                  BiFunction<Integer, Integer, Integer> operation,
+                                  BiFunction<Integer, Integer, Integer> reduction,
+                                  int accumulator) {
+
+    for (var row = 0; row < input.length - lineSize; row++) {
+      for (var col = 0; col < input[row].length - lineSize; col++) {
+        if (col >= lineSize) {
+          accumulator = reduction.apply(reduceLeftDiagonalLine(input, row, col, lineSize, operation), accumulator);
+        }
+
+        accumulator = reduction.apply(reduceVerticalLine(input, row, col, lineSize, operation), accumulator);
+        accumulator = reduction.apply(reduceHorizontalLine(input, row, col, lineSize, operation), accumulator);
+        accumulator = reduction.apply(reduceRightDiagonalLine(input, row, col, lineSize, operation), accumulator);
+      }
+    }
+
+    return accumulator;
+  }
+
+  private static int reduceVerticalLine(int[][] input, int row, int col, int lineSize, BiFunction<Integer, Integer, Integer> operation) {
+    return matrixReduce(input, row, col, lineSize, operation, i -> input[row + i][col]);
+  }
+
+  private static int reduceHorizontalLine(int[][] input, int row, int col, int lineSize, BiFunction<Integer, Integer, Integer> operation) {
+    return matrixReduce(input, row, col, lineSize, operation, i -> input[row][col + i]);
+  }
+
+  private static int reduceRightDiagonalLine(int[][] input, int row, int col, int lineSize, BiFunction<Integer, Integer, Integer> operation) {
+    return matrixReduce(input, row, col, lineSize, operation, i -> input[row + i][col + i]);
+  }
+
+  private static int reduceLeftDiagonalLine(int[][] input, int row, int col, int lineSize, BiFunction<Integer, Integer, Integer> operation) {
+    return matrixReduce(input, row, col, lineSize, operation, i -> input[row + i][col - i]);
+  }
+
+  private static int matrixReduce(int[][] input, int row, int col, int lineSize, BiFunction<Integer, Integer, Integer> operation, Function<Integer, Integer> direction) {
+    int accumulator = input[row][col];
+
+    for (int i = 1; i < lineSize; i++) {
+      accumulator = operation.apply(accumulator, direction.apply(i));
+    }
+
+    return accumulator;
+  }
+
+}
