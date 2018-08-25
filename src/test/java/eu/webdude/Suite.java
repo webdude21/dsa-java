f4eca90c2227adcfd06594ebb7e8aafed4cd5067
webdude21 <dimo.petrof@gmail.com>
2018-08-25 23:25:28 +0300

    add suite, remove duplicate solution

---
+++ src/test/java/eu/webdude/Suite.java
@@ -0,0 +1,10 @@
+package eu.webdude;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("eu.webdude.dsa")
+public class Suite {
+}
